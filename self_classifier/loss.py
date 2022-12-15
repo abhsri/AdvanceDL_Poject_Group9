7ba7b94bae5db9f9dacec8dbff3c8b6d58ea504f
Johan Ahnfalk <johan.ahnfalk@gmail.com>
2022-12-15 12:04:20 +0100

    Clean up and fixes to final code

--- self_classifier/loss.py
+++ self_classifier/loss.py
@@ -4,35 +4,48 @@ from tensorflow.keras.activations import softmax
 
 from tensorflow.math import log as tf_log
 from tensorflow.math import reduce_sum as tf_sum
-from tensorflow.math import scalar_mul as tf_smul
-
-from tensorflow.linalg import normalize as tf_norm
+from tensorflow.math import maximum as tf_max
 
 
 class SelfClassifier():
-    def __init__(self, n_batch: int, n_class: int, t_r: float, t_c: float):
+    def __init__(self, n_batch: int, n_class: int, t_r: float, t_c: float, eps=1e-8):
         self.N = n_batch
         self.C = n_class
         self.t_r = t_r
         self.t_c = t_c
+        self.eps = eps
 
     @tf.function
     def __call__(self, s1, s2):
         N, C, t_r, t_c = self.N, self.C, self.t_r, self.t_c
         # log_y_x1
         softmax_y_x1 = softmax(s1/t_r, axis=1)
-        norm_y_x1, _ = tf_norm(softmax_y_x1, ord=1, axis=0)
-        log_y_x1 = tf_log(N/C * norm_y_x1)
+
+        norm_y_x1 = self._normalize(softmax_y_x1, p=1, dim=0)
+        log_y_x1 = tf_log(N/C * norm_y_x1 + self.eps)
 
         # log_y_x2
         softmax_y_x2 = softmax(s2/t_r, axis=1)
-        norm_y_x2, _ = tf_norm(softmax_y_x2, ord=1, axis=0)
-        log_y_x2 = tf_log(N/C * norm_y_x2)
+        norm_y_x2 = self._normalize(softmax_y_x2, p=1, dim=0)
+        log_y_x2 = tf_log(N/C * norm_y_x2 + self.eps)
 
-        y_x1, _ = tf_norm(softmax(s1/t_c, axis=0), ord=1, axis=1)
-        y_x2, _ = tf_norm(softmax(s2/t_c, axis=0), ord=1, axis=1)
+        y_x1 = self._normalize(softmax(s1/t_c, axis=0), p=1, dim=1)
+        y_x2 = self._normalize(softmax(s2/t_c, axis=0), p=1, dim=1)
 
         l1 = - tf_sum(y_x2*log_y_x1, axis=-1) / N
         l2 = - tf_sum(y_x1 * log_y_x2, axis=-1) / N
-        L = tf_sum((l1 + l2) / 2)
+        l1_l2 = (l1 + l2) / 2
+        L = tf_sum(l1_l2)
         return L
+
+    def _normalize(self, tensor, p=2, dim=1):
+
+        norm = tf.norm(tensor, ord=p, axis=dim)
+        t_eps = tf.constant([self.eps])
+        if dim == 0:
+            norm_tensor = tensor / tf_max(norm, t_eps)
+        elif dim == 1:
+            norm_tensor = tensor / tf.reshape(tf_max(norm, t_eps), shape=[-1,1])
+            
+        return norm_tensor
+        
--- self_classifier/model.py
+++ self_classifier/model.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 
-from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, InputLayer, Rescaling,\
+Dense, Flatten, BatchNormalization, Dropout, LayerNormalization, LeakyReLU, Lambda
+
 from tensorflow.keras.activations import softmax
 from tensorflow.keras import Model
+
 
 
 class CustomModel(Model):
@@ -21,66 +24,102 @@ class CustomModel(Model):
 
     def to_prediction(self, predictions):
         """Function that takes softmax and argmax of the predictions"""
-        top_class = tf.argmax(predictions, axis=1)
+        pred_softmax = softmax(predictions, axis=0)
+        top_class = tf.argmax(pred_softmax, axis=1)
         return top_class
 
 
-class DynamicModel(CustomModel):
-    def __init__(self, input_shape, output_shape, n_layers=2, n_nodes=128,
-                 activation=tf.keras.layers.ReLU(), batch_norm=True,
-                  if_conv_layer=True, final_layer_bias=True, final_layer_activation=True):
+class DynamicModel(sc.CustomModel):
+    def __init__(self, input_shape, n_layers=2, n_nodes=128, 
+                 activation="relu", batch_norm=True):
         super(DynamicModel, self).__init__()
         # Input layers
-        self.model = tf.keras.models.Sequential([tf.keras.Input(shape=input_shape)])
-        if if_conv_layer:
-          self.model.add(tf.keras.layers.Conv2D(32, 3, activation=activation))
-        self.model.add(tf.keras.layers.Flatten())
-        # Hidden Layers
-        for _ in range(n_layers):
-            self.model.add(tf.keras.layers.Dense(n_nodes))
-            self.model.add(activation)
+        self.model = tf.keras.models.Sequential([
+            InputLayer(input_shape=input_shape),
+            Rescaling(1./255),
+            Conv2D(16, 3, activation=activation),
+            Flatten(),
+            BatchNormalization(),
+            Dropout(0.5)
+            #tf.keras.layers.
+        ])
+        # Hidden Layers 
+        for i in range(n_layers):
+            self.model.add(Dense(n_nodes, activation=activation))
             if batch_norm:
-                self.model.add(tf.keras.layers.BatchNormalization())
+                self.model.add(BatchNormalization())
+                #self.model.add(tf.keras.layers.Dropout(0.25))
 
         # Output layer
-        self.model.add(tf.keras.layers.Dense(output_shape, use_bias=final_layer_bias))
-        if final_layer_activation:
-            self.model.add(tf.keras.layers.Activation("softmax"))
-
-
-class PretrainedModel(CustomModel):
-    def __init__(self, pre_model, activation, input_shape, output_shape):
-        super(PretrainedModel, self).__init__()
-
-        if pre_model == "ResNet50":
-            base_model = tf.keras.applications.resnet50.ResNet50(
-                weights='imagenet', include_top=False,
-                input_shape=input_shape)
-
-        elif pre_model == "EfficientNetB0":
-            base_model = tf.keras.applications.efficientnet.EfficientNetB0(
-                weights='imagenet', include_top=False,
-                input_shape=input_shape)
-
-        elif pre_model == "EfficientNetB1":
-            base_model = tf.keras.applications.efficientnet.EfficientNetB1(
-                weights='imagenet', include_top=False,
-                input_shape=input_shape)
-
-        elif pre_model == "MobileNet":
-            base_model = tf.keras.applications.mobilenet.MobileNet(
-                weights='imagenet', include_top=False,
-                input_shape=input_shape)
-
-        for layer in base_model.layers:
-            layer.trainable = False
+        self.model.add(Dense(n_nodes))
+        self.model.add(BatchNormalization())
+        self.model.add(LeakyReLU())
+        
+        self.model.add(Lambda(lambda x: tf.math.l2_normalize(x)))
+        self.model.add(Dense(10, use_bias=True))
+ 
+ 
+class DynamicModelCnn(sc.CustomModel):
+    def __init__(self, input_shape, n_classes=10, dropout=0.5):
+        super(DynamicModelCnn, self).__init__()
+        # Input layers
+        model = tf.keras.models.Sequential()
+        model.add(InputLayer(input_shape=input_shape)),
+        model.add(Rescaling(1./255)),
+        model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(BatchNormalization())
+        model.add(Dropout(dropout))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(BatchNormalization())
+        model.add(Dropout(dropout))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
 
         # Output layer
-        x = tf.keras.layers.Flatten()(base_model.output)
-        x = tf.keras.layers.Dense(1024, activation=activation)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Dense(128, activation=activation)(x)
-        predictions = tf.keras.layers.Dense(
-            output_shape, activation='softmax')(x)
+        model.add(Flatten())
+        model.add(BatchNormalization())
+        model.add(Dropout(dropout))
+        
+        model.add(Dense(128))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU())
+        
+        model.add(Lambda(lambda x: tf.math.l2_normalize(x)))
+        model.add(Dense(n_classes, use_bias=True))
+        
+        self.model = model
 
-        self.model = Model(inputs=base_model.input, outputs=predictions)
+class CNNModel(sc.CustomModel):
+    def __init__(self, input_shape, activation="relu", batch_norm=True):
+        super(CNNModel, self).__init__()
+        models = tf.keras.models.Sequential()
+        models.add(InputLayer(input_shape=input_shape))
+        models.add(Rescaling(1./255))
+        models.add(Conv2D(64, (5, 5),
+                          padding="same",
+                          activation="relu"))
+        models.add(MaxPooling2D(pool_size=(2, 2)))
+        models.add(Conv2D(128, (5, 5), padding="same",
+                          activation="relu"))
+
+        
+        models.add(MaxPooling2D(pool_size=(2, 2)))
+        models.add(Conv2D(256, (5, 5), padding="same",
+                          activation="relu"))
+
+        models.add(MaxPooling2D(pool_size=(2, 2)))
+        models.add(Flatten())
+        
+        models.add(Dense(128))
+        models.add(BatchNormalization())
+        models.add(LeakyReLU())
+        models.add(Dense(64))
+        models.add(BatchNormalization())
+        models.add(LeakyReLU())
+        
+        models.add(Lambda(lambda x: tf.math.l2_normalize(x)))
+        models.add(Dense(10, use_bias=True))
+
+        
+        self.model = models
